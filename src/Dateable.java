e3d7a4dc3cdae59ad5ffdac1cc6b51b04be0d12d
jeanine0927 <122774440+jeanine0927@users.noreply.github.com>
2023-02-01 12:43:59 +0800

    Update Dateable.java

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Dateable {
+import java.util.Scanner;
+public class Dateable {
+
+    public static void main(String[] args) {
+        Scanner age = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int yourage = age.nextInt();
+        int minage = (yourage/2) + 7;
+        System.out.println(yourage + " year olds should data somebody who is at least " + minage + " years old.");
+    }
 }
